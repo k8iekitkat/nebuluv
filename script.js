6763820729c57c2f221bc4afb87633897b369635
Kaitlyn Jao <kaitlynjao@gmail.com>
2025-04-13 04:52:43 -0700

    changed question answer

--- script.js
+++ script.js
@@ -154,8 +154,8 @@ const questions = {
     "2": {
         "question_text": "While traversing through a star system, you come across a mysterious nebula pulsing with energy. How do you interact with it?",
         "question_choices": {
-            "Reflective Sage: You stop to think about the deeper meanings and lessons the nebula might be able to offer": [3, ["Venus"]],
-            "Bold Explorer: You leap into the unknown, being eager and experienced to experience its raw energy and want to uncover its secrets": [3, ["Mars"]],
+            "Reflective Sage: You stop to think about the deeper meanings and lessons the nebula might be able to offer.": [3, ["Venus"]],
+            "Bold Explorer: You leap into the unknown, being eager and experienced to experience its raw energy and want to uncover its secrets.": [3, ["Mars"]],
             "Overthink Genie:  You deeply think about its energy and wonder what it potentially has to benefit you. ": [3, ["Uranus"]],
             "Hopeful Dreamer: You see the nebula as a symbol of infinite possibilities and want to embrace the encounter with optimism.": [3, ["Jupiter"]]
         }
@@ -167,7 +167,7 @@ const questions = {
         "question_choices": {
             "Double Dare: You enter the rift, having the only thought in your mind to move forward!": [4, ["Jupiter"]],
             "Spiriter: You've been waiting for something like this to happen, driven and excited by the thrill of what might be in there.": [4, ["Mars"]],
-            "Future Prayer: You want to embrace the rift, thinking of it as a symbol of boundless opportunity and renewal. Confident that it will hold positive changes for your future": [4, ["Uranus"]],
+            "Future Prayer: You want to embrace the rift, thinking of it as a symbol of boundless opportunity and renewal. Confident that it will hold positive changes for your future.": [4, ["Uranus"]],
             "Thoughtful Learner: You're excited to use this opportunity as a lesson to learn what's inside.": [4, ["Venus"]],
         }
     },
@@ -199,7 +199,7 @@ const questions = {
         "question_text": "You and your team finally land on an unknown planet. What is the first thing you do?",
         "question_choices": {
             "Observing Nerd: You immediately look and observe, analyzing the dust, temperature, etc.": [7, ["Venus"]],
-            "Buddy System: You can't explore without experiencing it with somebody. Makes it more enjoyable.": [7, ["Uranus"]],
+            "Buddy System: Grab a partner! You can't explore without experiencing it with somebody. Makes it more enjoyable.": [7, ["Uranus"]],
             "Priorities: You have to explore every inch, what if there are aliens on planet?": [7, ["Jupiter"]],
             "Faster: You run off, the thought of everything being unknown thrills you to the bone.": [7, ["Mars"]],
         }
@@ -242,7 +242,7 @@ const questions = {
     "10": {
         "question_text": "It's time to head back to the ship. What would be your regret?",
         "question_choices": {
-            "Memorable thoughts: That you didn't get to write down every thought you had": [0, ["Venus"]],
+            "Memorable thoughts: That you didn't get to write down every thought you had.": [0, ["Venus"]],
             "Unseen: How you wish you could've used this chance to bond with everyone more.": [0, ["Uranus"]],
             "New beginnings:No regrets, everything will come to happen anyways. ": [0, ["Jupiter"]],
             "Thriller: To explore more. ": [0, ["Mars"]],

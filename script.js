a240a12e9706097532d618b41ea7da966314686c
Kentalope22 <69648019+Kentalope22@users.noreply.github.com>
2025-04-12 21:03:49 -0700

    button functions in js

---
+++ script.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById('start-button')
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+    console.log('Started')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}

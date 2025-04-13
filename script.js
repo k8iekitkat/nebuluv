4f61335d41f70da983dcab55db5fa0394aa211ca
Kentalope22 <69648019+Kentalope22@users.noreply.github.com>
2025-04-12 22:13:30 -0700

    just like 2 lines

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const startButton = document.getElementById('start-button')
-
+const questionContainerElement = document.getElementById('question-container')
 startButton.addEventListener('click', startGame)
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answer-buttons')
 
 function startGame() {
     console.log('Started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+function showQuestion() {
+
 }
 
 function setNextQuestion() {
@@ -13,3 +21,4 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+

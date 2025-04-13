66a8dd2a0a2ebeba47996d5848a371c8b395bf81
Kaitlyn Jao <kaitlynjao@gmail.com>
2025-04-12 23:13:44 -0700

    questions 7-9

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const questions = {
     },
 
     // question 5
-      "5": {
+    "5": {
         "question_text": "While everyone is sleeping, you notice something unfamiliar coming your way. What do you do?",
         "question_choices": {
             "Don't Care: You ignore it, worst case scenario it makes a small dent.": [6, ["Jupiter"]],
@@ -78,16 +78,48 @@ const questions = {
         }
     },
 
-      // question 6
-      "6": {
+    // question 6
+    "6": {
         "question_text": "You and your team finally land on an unknown planet. What is the first thing you do?",
         "question_choices": {
             "Observing Nerd: You immediately look and observe, analyzing the dust, temperature, etc.": [7, ["Venus"]],
             "Buddy System: You can't explore without experiencing it with somebody. Makes it more enjoyable.": [7, ["Uranus"]],
-            "Priorities: You have to explore every inch, what if there are aliens on planet?": [6, ["Jupiter"]],
-            "Faster: You run off, the thought of everything being unknown thrills you to the bone.": [6, ["Mars"]],
+            "Priorities: You have to explore every inch, what if there are aliens on planet?": [7, ["Jupiter"]],
+            "Faster: You run off, the thought of everything being unknown thrills you to the bone.": [7, ["Mars"]],
         }
     },
 
+    // question 7
+    "7": {
+        "question_text": "During your adventure on this new planet, you get lost. How do you feel?",
+        "question_choices": {
+            "Who Cares: You'll find them eventually.": [8, ["Jupiter"]],
+            "Tunnel Vision: This marking on this rock is too fascinating.": [8, ["Venus"]],
+            "Solo: Awesome, the thought of being alone excites you.": [8, ["Mars"]],
+            "Help!: You're absolutely terrified. who knows what's around here?!": [8, ["Uranus"]],
+        }
+    },
+    
+    // question 8
+    "8": {
+        "question_text": "After about an hour, you're still alone and have encountered a liquid that appears to look like water. What do you do?",
+        "question_choices": {
+            "Patience is Key: You would rather wait until you have fully deduced if it's ok to drink it or not.": [9, ["Venus"]],
+            "Lets do it: The thought of the unknown intruiges you. You take a sip.": [9, ["Mars"]],
+            "Partnership!: Thirstiness is not important, finding someone is. ": [9, ["Uranus"]],
+            "Why not: It looks close enough, lets try it.": [9, ["Jupiter"]],
+        }
+    },
 
-};
+    // question 9
+    "9": {
+        "question_text": "Some time goes by and you finally found your peers. How did you find them?",
+        "question_choices": {
+            "Mapper: You found them through your own deduction skills.": [10, ["Venus"]],
+            "Isolation: You sat in place until someone found you.": [10, ["Uranus"]],
+            "Oh well: Random chance. You knew you'd find them eventually.": [10, ["Jupiter"]],
+            "Random Chance: You were adventuring randomly and just happened to encounter them.": [10, ["Mars"]],
+        }
+    },
+}
+        
